420c0245591b1eebc1e3809434995af3103b8b91
Gourav Malviya <gouravmalviya445@gmail.com>
2024-03-03 16:11:07 +0530

    Updating

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -5,7 +5,7 @@ const copyBtn = document.getElementById('copy-btn');
 
 function randomPasswordGenerator(){
     let pass = '';
-    let str = 'ABCDEGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz0123456789~!@#$%^&*-_=+`';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz0123456789~!@#$%^&*-_=+`';
     for(let i = 1; i <= 10; i++){
         const char = Math.floor(Math.random() * str.length + 1);
         pass += str.charAt(char);
